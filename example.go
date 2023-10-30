e4afeba0e6988aeac64dade333ec6cfd14cc3fb9
shimada <yoshihiro.shimada@photosynth.co.jp>
2023-10-30 09:32:38 +0900

    :add: client credentials flow sample

--- example.go
+++ example.go
@@ -1,6 +1,16 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-oauth2/oauth2/v4/errors"
+	"github.com/go-oauth2/oauth2/v4/manage"
+	"github.com/go-oauth2/oauth2/v4/models"
+	"github.com/go-oauth2/oauth2/v4/server"
+	"github.com/go-oauth2/oauth2/v4/store"
+)
 
 func main() {
 	r := gin.Default()
@@ -9,6 +19,39 @@ func main() {
 			"message": "pong",
 		})
 	})
+
+	manager := manage.NewDefaultManager()
+	manager.MustTokenStorage(store.NewMemoryTokenStore())
+
+	clientStore := store.NewClientStore()
+	clientStore.Set("000000", &models.Client{
+		ID:     "000000",
+		Secret: "999999",
+		Domain: "http://localhost",
+	})
+	manager.MapClientStorage(clientStore)
+
+	srv := server.NewDefaultServer(manager)
+	srv.SetAllowGetAccessRequest(true)
+	srv.SetClientInfoHandler(server.ClientFormHandler)
+	srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
+		log.Println("Internal Error:", err.Error())
+		return
+	})
+
+	srv.SetResponseErrorHandler(func(re *errors.Response) {
+		log.Println("Response Error:", re.Error.Error())
+	})
+
+	r.POST("/token", func(c *gin.Context) {
+		err := srv.HandleTokenRequest(c.Writer, c.Request)
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{"status": "no value"})
+		}
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
 	r.Run() // 0.0.0.0:8080 でサーバーを立てます。
 }
-
