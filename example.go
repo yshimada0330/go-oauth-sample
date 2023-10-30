cd8d01065034a2cbff829d07cea74a2afa33dcb7
shimada <yoshihiro.shimada@photosynth.co.jp>
2023-10-30 10:15:24 +0900

    :add: Basic Auth

--- example.go
+++ example.go
@@ -32,8 +32,23 @@ func main() {
 	manager.MapClientStorage(clientStore)
 
 	srv := server.NewDefaultServer(manager)
-	srv.SetAllowGetAccessRequest(true)
-	srv.SetClientInfoHandler(server.ClientFormHandler)
+	// srv.SetAllowGetAccessRequest(true)
+
+	// NOTE: Authorization: Basic による認証の場合
+	// $ curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -H "Authorization: Basic MDAwMDAwOjk5OTk5OQ==" -d "grant_type=client_credentials" http://localhost:8080/token
+	srv.SetClientInfoHandler(server.ClientBasicHandler)
+
+	// NOTE: POSTパラメータ or GETパラメータ による認証の場合
+	// $ curl --request POST \
+	// --url 'http://localhost:8080/token' \
+	// --header 'content-type: application/x-www-form-urlencoded' \
+	// --data grant_type=client_credentials \
+	// --data client_id=000000 \
+	// --data client_secret=999999
+	//
+	// $ curl -X POST 'http://localhost:8080/token?grant_type=client_credentials&client_id=000000&client_secret=999999'
+	// srv.SetClientInfoHandler(server.ClientFormHandler)
+
 	srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
 		log.Println("Internal Error:", err.Error())
 		return
@@ -46,11 +61,9 @@ func main() {
 	r.POST("/token", func(c *gin.Context) {
 		err := srv.HandleTokenRequest(c.Writer, c.Request)
 		if err != nil {
-			c.JSON(http.StatusOK, gin.H{"status": "no value"})
+			c.JSON(http.StatusOK, gin.H{"message": err.Error()})
+			return
 		}
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
 	})
 
 	r.Run() // 0.0.0.0:8080 でサーバーを立てます。
